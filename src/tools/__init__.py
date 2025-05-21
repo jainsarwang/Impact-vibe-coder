d396e049af33a87bea0831db3a6702bfd76db89f
Sarwang Jain <jainsarwang@gmail.com>
2025-05-21 22:24:17 +0530

    Added Read File tool

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -5,6 +5,7 @@ from .search import tavily_tool
 from .bash_tool import bash_tool
 from .browser import browser_tool
 from .create_zip import project_zip_tool
+from .read_file import read_file_tool
 
 __all__ = [
     "bash_tool",
@@ -14,4 +15,5 @@ __all__ = [
     "write_file_tool",
     "browser_tool",
     "project_zip_tool",
+    "read_file_tool"
 ]
