78e255b3fd0d5a5dc9ceb2666879d92351a6ac4a
Sarwang Jain <jainsarwang@gmail.com>
2025-06-09 15:15:34 +0530

    Added checlist to figma coder

--- src/graph/nodes.py
+++ src/graph/nodes.py
@@ -763,7 +763,8 @@ def figma_coder_node(state: State) -> Command[Literal["coder_master"]]:
         
     current_generated_files = state.get('generated_files', [])
     state['generated_files'] = current_generated_files + [parsed_instruction.get("file", "")]
-
+    state.get('checklist_manager').mark_file_created(parsed_instruction.get("file", ""))
+    
     return Command(
         update={
             "messages": [
